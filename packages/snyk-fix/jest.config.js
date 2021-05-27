5f2ad836b02494360aeb82b1f1c2e8466e95d695
Jahed Ahmed <jahed@snyk.io>
2021-05-27 10:26:01 +0000

    chore: upgrade to jest@27

--- packages/snyk-fix/jest.config.js
+++ packages/snyk-fix/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
   transform: {}, // ignore .babelrc file
   collectCoverage: false, // not collecting coverage for now
   collectCoverageFrom: ['src/**/*.ts'],
